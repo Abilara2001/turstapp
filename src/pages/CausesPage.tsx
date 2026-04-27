0cb749a42f97867b8c9932a1f182f5b6c2a5122f
Abirami <144773265+Abilara2001@users.noreply.github.com>
2026-04-27 04:22:28 +0000

    Feat: Header changed

--- src/pages/CausesPage.tsx
+++ src/pages/CausesPage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { causes } from '../data/causes';
 import { CauseCard } from '../components/Card/CauseCard';
 
-export function CausesPage(): JSX.Element {
+export function CausesPage(): React.ReactElement {
 	return (
 		<div className="section">
-			<h2 className="section-title">Our Causes</h2>
+			<h2 className="section-title">Our Causes(oc)</h2>
 			<div className="grid">
 				{causes.map(cause => (
 					<CauseCard key={cause.id} cause={cause} />
